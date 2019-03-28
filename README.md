8ce02dfaa46b52d659d3ca425364711818290655
LeakMania-v6 <48015201+LeakMania-v6@users.noreply.github.com>
2019-03-28 22:06:50 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Apache2-MySQL-Auto-Backup-Script
 Just a simple auto-backup script for Apache2 and MySQL.
+<br><br>
+Requirements:
+<br>
+```
+apt-get install zip curl screen
+```
+<br><br>
+Launch this script with these commands:
+<br>
+```
+git clone https://github.com/LeakMania-v6/Apache2-MySQL-Auto-Backup-Script.git
+chmod 777 backup.sh
+screen -dmS autobackup ./backup.sh
+```
---
+++ backup.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+date=`date '+[%Y_%m_%d]<->[%H-%M-%S]'`
+backup_name=database_backup_$date.zip
+site_backup_name=site_backup_$date.zip
+
+#Timeout between every backups (in seconds).
+timeout=86400
+
+#Database login informations.
+db_user='username'
+db_password='password'
+
+#Backup password.
+#It's recommended to use an very strong password here.
+backup_pw='very_strong_password'
+
+#Here you need to put your anonfile token.
+#Get your anonfile token here: https://anonfile.com/docs/api
+#Only available for registered peoples.
+anonfile_token='token'
+
+#List all websites you want to backup here.
+#Exemple: sites=("site1" "site2")
+sites=("public_html")
+databases=("main_database")
+
+#Folder where you store your website(s)
+sites_path='/var/www'
+sites_paths=''
+for str in "${sites_paths[@]}"
+do
+	sites_paths+=' '$sites_path'/'$str
+done
+zip --password $backup_pw $site_backup_name -r$sites_paths
+
+files=''
+for str in "${databases[@]}"
+do
+	mysqldump --user=$db_user --password=$db_password --databases $str > $str.sql
+	files+=$str'.sql '
+done
+
+for str in "${databases[@]}"
+do
+	mysqldump --user=$db_user --password=$db_password --databases $str > $str.sql
+	files+=$str'.sql '
+done
+
+mkdir backups
+mv *.sql backups
+zip --password $backup_pw $backup_name -r backups
+rm -rf backups
+
+curl -F "file=@$backup_name" https://anonfile.com/api/upload?token=$anonfile_token
+curl -F "file=@$site_backup_name" https://anonfile.com/api/upload?token=$anonfile_token
+
+rm $site_backup_name
+rm $backup_name
+
+sleep $timeout
+
+./backup.sh
+
+echo $files
